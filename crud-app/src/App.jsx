77281d41a91a348cfc0be76e298e14c5bb860708
GNikhilSS <nikhilchowdarygarapati@gmail.com>
2025-07-29 17:48:58 +0530

    1: setup

---
+++ crud-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button class="btn btn-primary">Button</button>
+
+    </>
+  )
+}
+
+export default App
