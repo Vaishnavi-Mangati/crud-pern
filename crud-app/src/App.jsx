c18b7a2d71b97b0e4304048b5e470a1f60712841
Vaishnavi-Mangati <vaishnavimangati@gmail.com>
2025-07-31 22:39:50 +0530

    03: table setup

--- crud-app/src/App.jsx
+++ crud-app/src/App.jsx
@@ -1,11 +1,31 @@
+import { use, useState } from 'react'
 import './App.css'
+import ModalForm from './components/ModalForm'
 import Navbar from './components/Navbar'
+import Tablelist from './components/Tablelist'
 
 function App() {
+  const [isOpen, setisOpen] = useState(false);
+  const [ModalMode, setModalMode] = useState('add');
+
+  const handleOpen = (mode) =>{
+    setisOpen(true);
+  }
+
+  const handleSubmit = () =>{
+    if(ModalMode === 'add'){
+      console.log("modal mode add")
+    }
+    else{
+      console.log("modal mode edit")
+    }
+  }
 
   return (
     <>
-    <Navbar/>
+    <Navbar onOpen = {() => handleOpen('add')}/>
+    <Tablelist />
+    <ModalForm isOpen = {isOpen} onClose = {() => setisOpen(flase)}/>
     </>
   )
 }
